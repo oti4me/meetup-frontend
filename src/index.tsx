9f720e41bb910f34415d8f128ce52fefa45c274a
Henry Otighe <oti4me@Henry-Otighe-MacBook-Pro.local>
2020-08-20 16:26:23 +0100

    setup react with webpack and typescript

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1>Hello, welcome to Meetup Ng!</h1>;
+};
+
+// render the app to the page
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', 'ts', 'tsx'],
+  },
+};
